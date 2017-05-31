b92a9b7ca9c90b44ed61b18b52fa558ed3986c00
Kartik Gola <krtkgla@gmail.com>
2017-06-01 00:08:52 +0530

    Added static Todos

---
+++ app/components/Todo.jsx
@@ -0,0 +1,18 @@
+let React = require('react');
+
+let Todo = React.createClass({
+
+  render : function(){
+
+    let {id, text} = this.props;
+
+    return (
+      <div>
+        {id}. {text}
+      </div>
+    )
+  }
+
+});
+
+module.exports = Todo;
--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -1,16 +1,53 @@
 
 let React = require('react');
+let TodoList = require('TodoList');
 
 let TodoApp = React.createClass({
 
+  getInitialState : function() {
+    return {
+      todos : [
+        {
+          id : 1,
+          text : 'Jog around colony'
+        },
+        {
+          id : 2,
+          text : 'Wash clothes'
+        },
+        {
+          id : 3,
+          text : 'Do some DP problems'
+        },
+        {
+          id : 4,
+          text : 'Learn React.js'
+        }
+      ]
+    };
+  },
+
   render : function() {
+
+    let {todos} = this.state;
+
     return (
-      <div className="row">
-        <div className="small-2 small-centered columns">
-          <h1> TodoApp </h1>
+      <div>
+
+        <div className="row">
+          <div className="small-2 small-centered columns">
+            <h1> TodoApp </h1>
+          </div>
         </div>
+
+        <div className="row">
+          <div className="small-4 small-centered columns">
+            <TodoList todos={todos} />
+          </div>
+        </div>
+
       </div>
-    )
+    );
   }
 
 });
