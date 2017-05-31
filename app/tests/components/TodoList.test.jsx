6b83588ce8999a045cc112e999e6e139f8c4bfc9
Kartik Gola <krtkgla@gmail.com>
2017-06-01 00:38:45 +0530

    Added Karma tests

---
+++ app/tests/components/TodoList.test.jsx
@@ -0,0 +1,33 @@
+
+let React = require('react');
+let ReactDOM = require('react-dom');
+let TestUtils = require('react-addons-test-utils');
+let expect = require('expect');
+let $ = require('jquery');
+
+let TodoList = require('TodoList');
+let Todo = require('Todo');
+
+describe('TodoList', () => {
+  it ('should exist', () => {
+    expect(TodoList).toExist();
+  });
+
+  it('should render one Todo component for each todo item', () => {
+    var todos = [
+      {
+        id : 1,
+        text : 'Do something'
+      },
+      {
+        id : 2,
+        text : 'Do something 2'
+      }
+    ];
+
+    var todoList = TestUtils.renderIntoDocument(<TodoList todos={todos} />);
+    var todosComponents = TestUtils.scryRenderedComponentsWithType(todoList, Todo);
+
+    expect(todosComponents.length).toBe(todos.length);
+  });
+});
