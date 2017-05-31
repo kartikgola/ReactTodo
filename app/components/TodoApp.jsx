db837906bf30dc89d52c0ff3d985bc86ef80ada4
Kartik Gola <krtkgla@gmail.com>
2017-05-31 23:44:12 +0530

    Added TodoApp main

--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -4,7 +4,13 @@ let React = require('react');
 let TodoApp = React.createClass({
 
   render : function() {
-
+    return (
+      <div className="row">
+        <div className="small-2 small-centered columns">
+          <h1> TodoApp </h1>
+        </div>
+      </div>
+    )
   }
 
 });
